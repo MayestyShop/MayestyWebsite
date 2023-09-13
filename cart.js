ff0edda6cf11baecf3cb4d8a8430bcc6c42cee52
MayestyShop <144959289+MayestyShop@users.noreply.github.com>
2023-09-13 23:07:14 +0200

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,8 @@
+/*function menuClicked(){
+    document.querySelector('#menu').style.display= 'block';
+
+  }
+  function menuClosed() {
+    document.querySelector('#menu').style.display= 'none';
+  }
+*/
