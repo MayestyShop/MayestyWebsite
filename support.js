6825f54c7e708128e2e6181e1ba2ecd5e7be3ac0
MayestyShop <144959289+MayestyShop@users.noreply.github.com>
2023-11-19 22:20:36 +0100

    Update support.js

--- support.js
+++ support.js
@@ -1,30 +1,30 @@
 document.querySelector('#Header1').addEventListener('click', function() {
-    window.location.href = '/Users/carsten/VSCode/Website/code/index.html';
+    window.location.href = 'index';
   });
   
 document.querySelector('#Header2').addEventListener('click', function() {
-    window.location.href = '/Users/carsten/VSCode/Website/code/shop.html';
+    window.location.href = 'shop';
   });
   
 document.querySelector('#Header3').addEventListener('click', function() {
-    window.location.href = '/Users/carsten/VSCode/Website/code/collections.html';
+    window.location.href = 'collections';
   });
   
 document.querySelector('#Header4').addEventListener('click', function() {
-    window.location.href = '/Users/carsten/VSCode/Website/code/support.html';
+    window.location.href = 'support';
   });
 
 q1 = 0;
 document.querySelector('#faq1').addEventListener('click', function() {
     if (q1 == 0){
         document.getElementById('faq1drop').style.display = "block";
-        document.getElementById('faq1img').src ="/Users/carsten/VSCode/Website/files/arrow upside.png";
+        document.getElementById('faq1img').src ="arrow upside.png";
         q1 += 1;
         return
     }
     if (q1 == 1){
         document.getElementById('faq1drop').style.display = "none";
-        document.getElementById('faq1img').src ="/Users/carsten/VSCode/Website/files/arrow.png";
+        document.getElementById('faq1img').src ="arrow.png";
         q1 -= 1;
         return
     }
@@ -34,13 +34,13 @@ q2 = 0;
 document.querySelector('#faq2').addEventListener('click', function() {
     if (q2 == 0){
         document.getElementById('faq2drop').style.display = "block";
-        document.getElementById('faq2img').src ="/Users/carsten/VSCode/Website/files/arrow upside.png";
+        document.getElementById('faq2img').src ="arrow upside.png";
         q2 += 1;
         return
     }
     if (q2 == 1){
         document.getElementById('faq2drop').style.display = "none";
-        document.getElementById('faq2img').src ="/Users/carsten/VSCode/Website/files/arrow.png";
+        document.getElementById('faq2img').src ="arrow.png";
         q2 -= 1;
         return
     }
@@ -50,13 +50,13 @@ q3 = 0;
 document.querySelector('#faq3').addEventListener('click', function() {
     if (q3 == 0){
         document.getElementById('faq3drop').style.display = "block";
-        document.getElementById('faq3img').src ="/Users/carsten/VSCode/Website/files/arrow upside.png";
+        document.getElementById('faq3img').src ="arrow upside.png";
         q3 += 1;
         return
     }
     if (q3 == 1){
         document.getElementById('faq3drop').style.display = "none";
-        document.getElementById('faq3img').src ="/Users/carsten/VSCode/Website/files/arrow.png";
+        document.getElementById('faq3img').src ="arrow.png";
         q3 -= 1;
         return
     }
@@ -66,13 +66,13 @@ q4 = 0;
 document.querySelector('#faq4').addEventListener('click', function() {
     if (q4 == 0){
         document.getElementById('faq4drop').style.display = "block";
-        document.getElementById('faq4img').src ="/Users/carsten/VSCode/Website/files/arrow upside.png";
+        document.getElementById('faq4img').src ="arrow upside.png";
         q4 += 1;
         return
     }
     if (q4 == 1){
         document.getElementById('faq4drop').style.display = "none";
-        document.getElementById('faq4img').src ="/Users/carsten/VSCode/Website/files/arrow.png";
+        document.getElementById('faq4img').src ="arrow.png";
         q4 -= 1;
         return
     }
@@ -82,13 +82,13 @@ q5 = 0;
 document.querySelector('#faq5').addEventListener('click', function() {
     if (q5 == 0){
         document.getElementById('faq5drop').style.display = "block";
-        document.getElementById('faq5img').src ="/Users/carsten/VSCode/Website/files/arrow upside.png";
+        document.getElementById('faq5img').src ="arrow upside.png";
         q5 += 1;
         return
     }
     if (q5 == 1){
         document.getElementById('faq5drop').style.display = "none";
-        document.getElementById('faq5img').src ="/Users/carsten/VSCode/Website/files/arrow.png";
+        document.getElementById('faq5img').src ="arrow.png";
         q5 -= 1;
         return
     }
@@ -98,13 +98,13 @@ q6 = 0;
 document.querySelector('#faq6').addEventListener('click', function() {
     if (q6 == 0){
         document.getElementById('faq6drop').style.display = "block";
-        document.getElementById('faq6img').src ="/Users/carsten/VSCode/Website/files/arrow upside.png";
+        document.getElementById('faq6img').src ="arrow upside.png";
         q6 += 1;
         return
     }
     if (q6 == 1){
         document.getElementById('faq6drop').style.display = "none";
-        document.getElementById('faq6img').src ="/Users/carsten/VSCode/Website/files/arrow.png";
+        document.getElementById('faq6img').src ="arrow.png";
         q6 -= 1;
         return
     }
@@ -114,13 +114,13 @@ q7 = 0;
 document.querySelector('#faq7').addEventListener('click', function() {
     if (q7 == 0){
         document.getElementById('faq7drop').style.display = "block";
-        document.getElementById('faq7img').src ="/Users/carsten/VSCode/Website/files/arrow upside.png";
+        document.getElementById('faq7img').src ="arrow upside.png";
         q7 += 1;
         return;
     }
     if (q7 == 1){
         document.getElementById('faq7drop').style.display = "none";
-        document.getElementById('faq7img').src ="/Users/carsten/VSCode/Website/files/arrow.png";
+        document.getElementById('faq7img').src ="arrow.png";
         q7 -= 1;
         return;
     }
@@ -130,13 +130,13 @@ q8 = 0;
 document.querySelector('#faq8').addEventListener('click', function() {
     if (q8 == 0){
         document.getElementById('faq8drop').style.display = "block";
-        document.getElementById('faq8img').src ="/Users/carsten/VSCode/Website/files/arrow upside.png";
+        document.getElementById('faq8img').src ="arrow upside.png";
         q8 += 1;
         return;
     }
     if (q8 == 1){
         document.getElementById('faq8drop').style.display = "none";
-        document.getElementById('faq8img').src ="/Users/carsten/VSCode/Website/files/arrow.png";
+        document.getElementById('faq8img').src ="arrow.png";
         q8 -= 1;
         return;
     }
@@ -146,13 +146,13 @@ q9 = 0;
 document.querySelector('#faq9').addEventListener('click', function() {
     if (q9 == 0){
         document.getElementById('faq9drop').style.display = "block";
-        document.getElementById('faq9img').src ="/Users/carsten/VSCode/Website/files/arrow upside.png";
+        document.getElementById('faq9img').src ="arrow upside.png";
         q9 += 1;
         return;
     }
     if (q9 == 1){
         document.getElementById('faq9drop').style.display = "none";
-        document.getElementById('faq9img').src ="/Users/carsten/VSCode/Website/files/arrow.png";
+        document.getElementById('faq9img').src ="arrow.png";
         q9 -= 1;
         return;
     }
