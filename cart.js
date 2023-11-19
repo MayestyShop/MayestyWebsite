4bc63732b77041846a7946c671dbe00aeae23a67
MayestyShop <144959289+MayestyShop@users.noreply.github.com>
2023-11-19 21:23:31 +0100

    Delete cart.js

--- cart.js
+++
@@ -1,8 +0,0 @@
-/*function menuClicked(){
-    document.querySelector('#menu').style.display= 'block';
-
-  }
-  function menuClosed() {
-    document.querySelector('#menu').style.display= 'none';
-  }
-*/
